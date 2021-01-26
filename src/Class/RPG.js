4e60bbf51dc02e8973ef93c195d5875c8b9c4dfd
zechaos031 <delrieu.adrien@gmail.com>
2021-01-27 00:56:05 +0100

    :sparkle: getItemInfo function

--- src/Class/RPG.js
+++ src/Class/RPG.js
@@ -1,61 +1,52 @@
 const Ora = require('ora')
-
+const fs = require('fs/promises')
+const Utils = require('../Utils/index')
 module.exports = class {
     constructor() {
+        this.utils = new Utils()
 
     }
-    init(){
-        const spinners = {
-            dbS : new Ora("Connexion à la base de donnée"),
-            verification : new Ora("Vérification des variables d'environement").start(),
-            discord : new Ora("Connexion à l'api Discord"),
-        };
-        setTimeout(function () {
-            spinners.verification.succeed('Oui')
-            spinners.dbS.start()
-            setTimeout(function () {
-                spinners.dbS.succeed('Oui')
-                spinners.discord.start()
-ZZ
-                setTimeout(function () {
-                    spinners.discord.succeed('Oui')
-
-                },2500)
-            },2500)
-        },2500)
-    }
-
-    getMonsterInfo(monster, level){
-        const fs =require('fs')
+    getItemInfo(type,name) {
         return new Promise((resolve, reject) => {
-            fs.readFile('./src/assets/JSON/monster.json', ((err, mst) => {
-                if(err) reject(err)
+            fs.readFile('./src/assets/JSON/object.json').then(function (obj) {
+                const objs = JSON.parse(obj)
+                resolve(objs[type][name])
+            }).catch(err =>{
+                reject(err)
+            })
+        })
+    }
 
+    getMonsterInfo(monster, level) {
+        return new Promise((resolve, reject) => {
+            fs.readFile('./src/assets/JSON/monster.json').then(function (mst) {
                 const monsters = JSON.parse(mst)
                 const stats = monsters.level[level][monster].stats
-                fs.readFile('./src/assets/JSON/loottable.json', ((err, lootlist) => {
-                    if(err) reject(err)
-
+                fs.readFile('./src/assets/JSON/loottable.json').then(function (lootlist) {
                     const loots = JSON.parse(lootlist)
                     const loottable = loots[monster]
-                    fs.readFile('./src/assets/JSON/object.json', ((err, obj) => {
-                        if(err) reject(err)
+                    fs.readFile('./src/assets/JSON/object.json').then(function (obj) {
                         const objs = JSON.parse(obj)
                         let loot = {}
-                        for (const lootobj of loottable){
-                            if(objs.loot[lootobj.name]) {
-                                Object.assign(loot,{
-                                    [lootobj.name]:{
-                                        info:objs.loot[lootobj.name]
-                                    }})
+                        for (const lootobj of loottable) {
+                            if (objs.loot[lootobj.name]) {
+                                Object.assign(loot, {
+                                    [lootobj.name]: [objs.loot[lootobj.name]]
+                                })
                             }
                         }
                         resolve({monster, stats, loot})
-                    }))
-                }))
-            }))
+                    }).catch((err) =>{
+                        reject(err)
+                    })
+                }).catch((err) =>{
+                    reject(err)
+                })
+            }).catch((err) =>{
+                reject(err)
+            })
         })
-
     }
+
 
 }
