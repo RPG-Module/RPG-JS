9ddf3958af2e4dbd3b65027336d01f94c7af9c72
zechaos031 <delrieu.adrien@gmail.com>
2021-01-27 11:08:59 +0100

    :rotating_light: Update test file

--- main.js
+++ main.js
@@ -2,8 +2,22 @@ const RPG = require('./src/Class/RPG')
 
 const rpg = new RPG()
 
-rpg.getMonsterInfo('Slime', 'medium').then(r => {
+rpg.monsters.getMonsterInfo('Slime').then(r =>{
+    console.log(r)
+}).catch((err)=>{
+    console.log(err)
 })
 
-rpg.getItemInfo("heal","Heal potion").then(r =>{
+
+rpg.items.getItemInfo("heal","Heal potion").then(r =>{
+    console.log(r)
+}).catch((err)=>{
+    console.log(err)
+})
+
+
+rpg.items.getMaterialArmor("Iron").then((r) =>{
+
+}).catch((err)=>{
+    console.log(err)
 })
