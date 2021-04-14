c9cf8859b1392b0d5e69efca0afe828f92709f46
zechaos031 <delrieu.adrien@gmail.com>
2021-04-15 00:29:09 +0200

    :wrench: Add Prototypes

--- src/utils/prototype.js
+++ src/utils/prototype.js
@@ -1,6 +1,4 @@
 Array.prototype.remove = function(strings) {
-    let found = this.indexOf(strings);
-
     while (this.indexOf(strings) !== -1) {
         this.splice(this.indexOf(strings), 1);
     }
